f74047b790d63bad95f1ee050d0b2107e42fc871
pedro <alexismoreauavila@gmail.com>
2024-10-13 10:52:48 +0200

    error fixed between two locals versions

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Api = axios.create({
-  baseURL: "http://192.168.1.14:5008/api",
+  baseURL: "http://172.31.99.243:5008/api",
 });
 
 Api.interceptors.request.use(
